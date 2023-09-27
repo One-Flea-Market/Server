96ed6d487cd7ce70a86b41701a16c0bab1d3cc78
kyungkiK <rudrlalsrl@gmail.com>
2023-09-27 23:45:32 +0900

    .

--- src/main/java/com/server/service/UserService.java
+++ src/main/java/com/server/service/UserService.java
@@ -19,14 +19,16 @@ public class UserService {
 
     /* 로그인 시 유저가 DB에 있는지 검증 */
     public UserDTO getOneUser (UserDTO dto) {
-        log.info("로그인 시도 후 유저 검증 시작");
+        log.info("로그인 시도 후 유저 검증 시작 {} ", dto);
 
         String encodedPwd = userMapper.selectEncPwd(dto);
         String rawPwd = dto.getStrPassword();
 
         if(passwordEncoder.matches(rawPwd, encodedPwd)) {
+            log.info("로그인 시도 후 유저 검증 시작 {} ", passwordEncoder.matches(rawPwd, encodedPwd));
             dto.setStrPassword(encodedPwd);
             UserDTO rspDto = userMapper.selectOneUser(dto);
+            log.info("로그인 시도 후 유저 검증 시작 {} ", rspDto);
             return rspDto;
         }
         return null;
