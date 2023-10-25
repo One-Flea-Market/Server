ea1d383820c0195b274ac84f2ed88eb96bd9574f
kyungkiK <rudrlalsrl@gmail.com>
2023-10-25 19:44:09 +0900

    Cross origin policy 수정

--- src/main/java/com/server/config/WebConfig.java
+++ src/main/java/com/server/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
     }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://client-p34zpc52f-capstone-team-market.vercel.app/");
+    }
+
 }
 
